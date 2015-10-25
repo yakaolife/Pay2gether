66f71ba3a2af391ef0f771015464675335aaf461
yakaolife <yakaolife@gmail.com>
2015-10-25 09:03:41 -0700

    Server: add braintree client token and transaction history from _pools.json

--- server.js
+++ server.js
@@ -15,20 +15,34 @@ console.log('Server listening on port 8081');
 // Socket.IO part
 var io = require('socket.io')(server);
 
-// var gateway=braintree.connect({
-// 	environment:braintree.Environment.Sandbox,
-// 	merchantId: "4s2bz328sgj8h75b",
-// 	publicKey:"t3xh48knfw2zxzv9",
-// 	privateKey:"0c52e5917f8e580e62b849b1774a9788"
-// });
+var gateway=braintree.connect({
+	environment:braintree.Environment.Sandbox,
+	merchantId: "4s2bz328sgj8h75b",
+	publicKey:"t3xh48knfw2zxzv9",
+	privateKey:"0c52e5917f8e580e62b849b1774a9788"
+});
 
 
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 io.on('connection', function(socket){
-	//console.log("A user connected");
 
-	//TODO: need invitation mechanics
+	//Braintree
+	socket.on('getClientToken', function(callback){
+		gateway.clientToken.generate({}, function(err, response){
+			//Probably should do error handling?
+			//response.send(response.clientToken);
+			console.log("Call Gateway with response "+response.clientToken);
+			if(response.clientToken){
+				//socket.emit('getClientToken', response.clientToken);
+				callback(response.clientToken, null);
+			}else{
+				console.error("Cannot get clientToken :(");
+				callback(null, "Cannot get client token");
+			}
+		});
+	});
+
 
 	//Pool
 
@@ -125,6 +139,31 @@ io.on('connection', function(socket){
 					console.log("Found the pool, now charge");
 					//TODO: do we need to convert from string to int?
 					pools[i].CurrentValue -=chargeValue;
+
+					//Using braintree for transaction
+
+					// gateway.paymentMethod.create({
+					//   customerId: "12345",
+					//   paymentMethodNonce: nonceFromTheClient
+					// }, function (err, result) { });
+
+					gateway.transaction.sale({
+						  amount: chargeValue,
+						  paymentMethodNonce: "fake-valid-nonce",
+						  options: {
+						    submitForSettlement: true
+						  }
+						}, function (error, result) {
+							console.log("Finished transaction");
+							if(error){
+								console.log("ERROR");
+							}else{
+								console.log("OK!");
+							}
+					});
+
+
+
 					callback(pools[i], error);
 				}
 			}
@@ -143,38 +182,6 @@ io.on('connection', function(socket){
 
 });
 
-
-// fs.readFile('_comments.json', 'utf8', function(err, comments) {
-// 	comments = JSON.parse(comments);
-// 	comments.push(comment);
-// 	fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function (err) {
-// 		io.emit('comments', comments);
-// 		callback(err);
-// 	});
-// });
-
-//Write POST in Node without library
-//http://stackoverflow.com/questions/6158933/how-to-make-an-http-post-request-in-node-js
-// app.post("/checkout", function (req, res) {
-//   var nonce = req.body.payment_method_nonce;
-//   // Use payment method nonce here
-// });
-
-// gateway.transaction.sale({
-//   amount: '10.00',
-//   paymentMethodNonce: nonceFromTheClient, //fake-valid-nonce
-// }, function (err, result) {
-// });
-
-// http.get("/client_token", function(request, response){
-// 	console.log("starting gateway");
-// 	gateway.clientToken.generate({}, function(err, response){
-// 		response.send(response.clientToken);
-// 		console.log("app.get is called?");
-// 	});
-// }).on('error', function(e) {
-//   console.log("Got error: " + e.message);
-// });
 
 
 
